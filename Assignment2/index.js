b79d7d82614fa9ad9a90611a4b4f68418e12c9c4
Neha <nehaamemon22@gmail.com>
2025-09-27 17:03:58 +0500

    Added JS variable File

---
+++ Assignment2/index.js
@@ -0,0 +1,43 @@
+// var UserName;
+// var fullNmame = "Naima Hanif";
+// var message = "Hello World";
+// alert(message);
+
+//student bio//
+// var name = "Neha";
+// var campus = "Zaitoon Ashrafi IT Park";
+// var course = "Web Development";
+
+// alert(name);
+// alert("My Campus is " + campus);
+// alert("Enroll course is " + course);
+
+
+
+//multiple Line Alert//
+// var food = "Pizza\nPizz\nPiz\nPi\nP";
+// alert(food)
+
+//email in Alert//
+// var email = "naimahanif8@gmail.com";
+// alert("My Email Address is " + email);
+
+
+
+//Book in Alert//
+// var book = "A smarter way to learn JavaScript.";
+// alert("I am trying to learn from the book " + book);
+
+
+//Text in Browser//
+// var text = "Yah ! I can write HTML content through JavaScript";
+// document.write(text);
+
+
+// //unicode Design//
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design);
+
+
+
