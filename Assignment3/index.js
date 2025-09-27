8643274e6e9a1e3cd58a5e255e8212a206d70363
Neha <nehaamemon22@gmail.com>
2025-09-27 17:23:31 +0500

    Added JS Number File

---
+++ Assignment3/index.js
@@ -0,0 +1,17 @@
+// var age = 20;  
+// alert("I am " + age + " years old");  
+
+// var visitCount = 10;
+// alert("You have visited this site" + visitCount + " times.")
+
+
+// var birthYear = 2002;  
+// document.write( "   My birth year is " + birthYear + "<br>");  
+// document.write( "    Data type of my declared variable is number");
+
+
+var visitorName = "John Doe";  
+var productTitle = "T-shirt";  
+var quantity = 5;  
+
+document.write( " "+ visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
