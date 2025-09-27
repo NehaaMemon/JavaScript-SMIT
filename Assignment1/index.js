dcb948580fcb96d69e4fc1cec45d99c8d3b2bb01
Neha <nehaamemon22@gmail.com>
2025-09-27 12:40:25 +0500

    Added JS alert File

---
+++ Assignment1/index.js
@@ -0,0 +1,20 @@
+
+  alert("Welcome to my website!");
+
+
+  {/* //Alert for valid password */}
+  alert("Please Enter a Valid password!");
+
+  {/* //Alert for message for website */}
+  alert("Welcome in Js website\n Happy Coding");
+
+    {/* //Alert for multiple alerts for website */}
+    alert("Welcome To JS");
+    alert("Happy Coding");
+
+     {/* //print in console */}
+     console.log("Hello Js ");
+
+
+
+
