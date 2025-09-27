d3e42fb69347ef3d3630aa7f381cb2a997dde8fd
Neha <nehaamemon22@gmail.com>
2025-09-27 17:43:18 +0500

    Added JS Illegal and legalr File

---
+++ assignment4/index.js
@@ -0,0 +1,7 @@
+var name, age, city;
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable <br>");
+document.write("Variable names are case sensitive <br>");
+document.write("Variable names should not be JS reserved keywords");
+
