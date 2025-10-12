4c4aec689ad358189fbfd5e2d3063bc130b0488a
Neha <nehaamemon22@gmail.com>
2025-10-12 09:48:04 +0500

    Added JS File

--- Assignment5/index.js
+++ Assignment5/index.js
@@ -30,7 +30,7 @@
 // document.write(`Modulus of ${num1} and ${num2} is ${result}<br>`);
 
 
-//Increment and Decrement
+// // Increment and Decrement
 // var input;
 // document.write(`Initial value is ${input}<br>`);
 // var input = 5;
@@ -44,31 +44,31 @@
 // var results = input % 3;
 // document.write(`Remainder is: ${results}<br>`);
 
-//Ticket//
+// // Ticket//
 
 // var ticket = 600;
 // var total = 600 * 4;
 // document.write(`Total cost to buy 4 tickets is: ${total} PKR<br>`);
 
 
-//Table//
+// // Table//
 // var table = 2;
 // for(var i = 1; i <= 10; i++){
 //     document.write(`${table} x ${i} = ${i*table}<br>`);
 // }
 
 
- //  Celsius temperature store
-    // let celsius = 25;
+//   // Celsius temperature store
+//     let celsius = 25;
 
    
-    // let fahrenheitFromC = (celsius * 9/5) + 32;
-    // document.write(`${celsius}°C is ${fahrenheitFromC}°F <br>`);
+//     let fahrenheitFromC = (celsius * 9/5) + 32;
+//     document.write(`${celsius}°C is ${fahrenheitFromC}°F <br>`);
 
-    // let fahrenheit = 77;
+//     let fahrenheit = 77;
 
-    // let celsiusFromF = (fahrenheit - 32) * 5/9;
-    // document.write(`${fahrenheit}°F is ${celsiusFromF}°C`);
+//     let celsiusFromF = (fahrenheit - 32) * 5/9;
+//     document.write(`${fahrenheit}°F is ${celsiusFromF}°C`);
 
 // var itemPrice = 6000;
 // var qty = 3;
@@ -89,12 +89,12 @@
 
 // document.write(`Total Cost of Your order is ${grand}<br>`);
 
-// var totalMarks = 500;
-// var obtainMarks = 480;
-// var percen = (obtainMarks / totalMarks) * 100;
-//  document.write(`Total Marks is ${totalMarks}<br>`);
-//  document.write(`Obtain Marks is ${obtainMarks}<br>`);
-//  document.write(`Percentage is ${percen}<br>`);
+var totalMarks = 500;
+var obtainMarks = 480;
+var percen = (obtainMarks / totalMarks) * 100;
+ document.write(`Total Marks is ${totalMarks}<br>`);
+ document.write(`Obtain Marks is ${obtainMarks}<br>`);
+ document.write(`Percentage is ${percen}<br>`);
 
 
 // var totalPkr = (10 * 104.80) + (25 * 28);
@@ -112,27 +112,27 @@
 
 
   
-//   var radius = 20;
+  // var radius = 20;
 
-//   var circumference = 2 * 3.142 * radius;
-//   document.write(`The circumference is ${circumference} <br>`);
+  // var circumference = 2 * 3.142 * radius;
+  // document.write(`The circumference is ${circumference} <br>`);
 
 
-//   var area = 3.142 * radius * radius;
-//   document.write(`The area is ${area}`);
+  // var area = 3.142 * radius * radius;
+  // document.write(`The area is ${area}`);
 
 
 
 
  
-  var snack = "Chocolate Chip Cookies";
-  var currentAge = 20;
-  var maxAge = 65;
-  var perDay = 2;
-  var totalSnacks = (maxAge - currentAge) * 365 * perDay;
+  // var snack = "Chocolate Chip Cookies";
+  // var currentAge = 20;
+  // var maxAge = 65;
+  // var perDay = 2;
+  // var totalSnacks = (maxAge - currentAge) * 365 * perDay;
 
-  document.write(`You will need ${totalSnacks} ${snack} to last you until the 
-    ripe old age of ${maxAge}`);
+  // document.write(`You will need ${totalSnacks} ${snack} to last you until the 
+  //   ripe old age of ${maxAge}`);
 
 
 
--- Assignment6/index.js
+++ Assignment6/index.js
@@ -68,3 +68,99 @@ alert("False");
  if("car" < "cat"){
 alert("car is smaller than cat");
  }
+
+
+    var subject1 = +prompt("Enter marks obtained in Subject 1:");
+    var subject2 = +prompt("Enter marks obtained in Subject 2:");
+    var subject3 = prompt("Enter marks obtained in Subject 3:");
+   var totalMarks = 300;
+
+    var obtainedMarks = subject1 + subject2 + subject3;
+    var percentage = (obtainedMarks / totalMarks) * 100;
+    var grade, remarks;
+
+    if (percentage >= 80) {
+      grade = "A-one";
+      remarks = "Excellent";
+    } else if (percentage >= 70) {
+      grade = "A";
+      remarks = "Good";
+    } else if (percentage >= 60) {
+      grade = "B";
+      remarks = "You need to improve";
+    } else {
+      grade = "Fail";
+      remarks = "Sorry";
+    }
+
+    document.write("<h3>Marks Sheet</h3>");
+    document.write("Total Marks: " + totalMarks + "<br>");
+    document.write("Marks Obtained: " + obtainedMarks + "<br>");
+    document.write("Percentage: " + percentage.toFixed(2) + "%<br>");
+    document.write("Grade: " + grade + "<br>");
+    document.write("Remarks: " + remarks + "<br><hr>");
+
+    // Guess Game =====
+    var secretNumber = 7; 
+    var userGuess = +prompt("Guess the secret number (1-10):");
+
+    if (userGuess === secretNumber) {
+      alert("Bingo! Correct answer");
+    } else if (userGuess + 1 === secretNumber) {
+      alert("Close enough to the correct answer");
+    } else {
+      alert("Try again!");
+    }
+
+    // Divisible by 3
+    var num = prompt("Enter a number to check if divisible by 3:");
+    if (num % 3 === 0) {
+      alert(num + " is divisible by 3.");
+    } else {
+      alert(num + " is not divisible by 3.");
+    }
+
+    //  Even or Odd
+    var checkNum = prompt("Enter a number to check even or odd:");
+    if (checkNum % 2 === 0) {
+      alert(checkNum + " is an even number.");
+    } else {
+      alert(checkNum + " is an odd number.");
+    }
+
+    // Temperature Message
+    var temp = prompt("Enter current temperature:");
+    if (temp > 40) {
+      alert("It is too hot outside.");
+    } else if (temp > 30) {
+      alert("The Weather today is Normal.");
+    } else if (temp > 20) {
+      alert("Today’s Weather is cool.");
+    } else if (temp > 10) {
+      alert("OMG! Today’s weather is so Cool.");
+    } else {
+      alert("It's freezing cold!");
+    }
+
+    //Simple Calculator 
+    var firstNum = prompt("Enter first number:");
+    var secondNum = prompt("Enter second number:");
+    var operation = prompt("Enter operation (+, -, *, /, %):");
+    var result;
+
+    if (operation === "+") {
+      result = firstNum + secondNum;
+    } else if (operation === "-") {
+      result = firstNum - secondNum;
+    } else if (operation === "*") {
+      result = firstNum * secondNum;
+    } else if (operation === "/") {
+      result = firstNum / secondNum;
+    } else if (operation === "%") {
+      result = firstNum % secondNum;
+    } else {
+      result = "Invalid operation!";
+    }
+
+    document.write("<h3>Calculator Result</h3>");
+    document.write(firstNum + " " + operation + " " + secondNum + " = " + result)
